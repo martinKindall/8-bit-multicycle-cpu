83ab606dcf94831ea907a768bb39b9b3caf7974a
Martin Cornejo <mart256@gmail.com>
2022-12-11 11:35:04 +0100

    Created regfile and tested it

---
+++ sim_1/new/RegFile_TB.sv
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+
+module RegFile_TB;
+
+    logic clk, we3;
+    logic [3:0] a1, a2, a3;
+    logic [7:0] wd3, rd1, rd2, rd1Test, rd2Test;
+    
+    logic [36:0] testvectors[1000:0];
+    logic [31:0] vectornum, errors;
+    
+    RegFile dut(clk, we3, a1, a2, a3, wd3, rd1, rd2);
+    
+    initial begin
+        $readmemh("RegFile.tv", testvectors);
+        vectornum = 0; errors = 0;
+        we3 <= 0; a1 <= 0; a2 <= 0; a3 <= 0; wd3 <= 0; #2;
+    end
+    
+    always begin
+        clk <= 1; #5;     
+        clk <= 0; #5;     
+    end
+    
+    always @(posedge clk) begin
+        #1; {we3, a1, a2, a3, wd3, rd1Test, rd2Test} = testvectors[vectornum];
+    end
+    
+    always @(negedge clk)
+        begin
+            if ({rd1, rd2} !== {rd1Test, rd2Test}) begin
+                $display("Error: output = %b (%b expected)", {rd1, rd2}, {rd1Test, rd2Test});
+                $display("vectornum %b", vectornum);
+            end
+            vectornum = vectornum + 1;
+            if (testvectors[vectornum] === 'x) begin 
+                $display("%d tests completed with %d errors", vectornum, errors);
+                $finish;
+            end
+        end
+
+endmodule
--- sources_1/new/RegFile.sv
+++ sources_1/new/RegFile.sv
@@ -1,26 +1,19 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/11/2022 10:55:13 AM
-// Design Name: 
-// Module Name: RegFile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module RegFile(
-
+    input clk, we3,
+    input [3:0] a1, a2, a3, 
+    input [7:0] wd3,
+    output [7:0] rd1, rd2
     );
+
+    logic [7:0] rf[15:0];
+
+    always_ff @(posedge clk)
+        if (we3)
+            rf[a3] <= wd3;
+
+    assign rd1 = (a1 != 0) ? rf[a1] : 0;
+    assign rd2 = (a2 != 0) ? rf[a2] : 0;
+
 endmodule
